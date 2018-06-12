2c765e148ae89836ad4459d88b38a1c8b1fc2c9f
Vinaymeldrum <36042425+Vinaymeldrum@users.noreply.github.com>
2018-06-12 11:59:41 -0400

    Changed fill

---
+++ AddYourWorkHere/Vinay_Meldrum_Agario/Blob.js
@@ -0,0 +1,30 @@
+function Blob(x, y, r) {
+  this.pos = createVector(x, y);
+  this.r = r;
+  this.vel = createVector(0, 0);
+
+
+  this.update = function() {
+    var newvel = createVector(mouseX-width/2, mouseY-height/2);
+    newvel.setMag(2);
+    this.vel.lerp(newvel, 0.6);
+    this.pos.add(this.vel);
+  }
+
+  this.eats = function(other) {
+    var d = p5.Vector.dist(this.pos, other.pos);
+    if (d < this.r + other.r) {
+      var sum = PI * this.r * this.r + PI *  other.r * other.r;
+      this.r = sqrt(sum / PI);
+      //this.r += other.r*0.2;;
+      return true;
+    } else {
+      return false;
+    }
+  } 
+  this.show = function() {
+    fill(154,74,214);
+    stroke(60);
+    ellipse(this.pos.x, this.pos.y, this.r*4, this.r*4);
+  }
+}
